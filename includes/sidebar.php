2692ca462829ca255c16257e7743ab2c9f309260
ettaoussi <ettaoussiilyas0.1@gmail.com>
2024-12-18 11:38:06 +0100

    ID-12 create page of categories and sous categories maagement

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,12 +2,12 @@
         <div class="p-4">
             <h2 class="text-yellow-300 text-xl font-bold mb-6">IT Thinken</h2>
             <ul class="flex flex-col space-y-4">
-                <li><a href="dashboard.php" class="text-yellow-300 hover:text-yellow-500 flex items-center">
+                <!-- <li><a href="dashboard.php" class="text-yellow-300 hover:text-yellow-500 flex items-center">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                     </svg>
                     Dashboard
-                </a></li>
+                </a></li> -->
                 <li><a href="./userss.php" class="text-yellow-300 hover:text-yellow-500 flex items-center">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
